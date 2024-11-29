e53922cc5311059a624a23e0d8fe942975b479be
kseen <korenevdn@gmail.com>
2024-11-29 05:08:08 +0300

    Update copyright notices in source files; add full license terms to lib.rs

--- src/dr.rs
+++ src/dr.rs
@@ -1,8 +1,7 @@
 /* 
 --------------------------------------------------------------------------------
 
-Copyright (c) 2019 Sebastian R. Verschoor. 
-<https://github.com/sebastianv89>
+Copyright (c) 2019 Sebastian R. Verschoor. <https://github.com/sebastianv89>
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
