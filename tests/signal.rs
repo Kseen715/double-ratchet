e6fbcb951697b40128fa6491d65e44b90728e9b5
kseen <korenevdn@gmail.com>
2024-11-29 04:46:48 +0300

    Refactor signal encryption and decryption logic; improve key derivation and padding handling

--- tests/signal.rs
+++ tests/signal.rs
@@ -1,29 +1,19 @@
-
-use aes::cipher::KeySizeUser;
-use aes::cipher::BlockSizeUser;
+use aes::cipher::{BlockSizeUser, KeySizeUser};
 use aes::Aes256;
-// use cbc::{Cbc, BlockMode, block_padding::Pkcs7};
-// use block_modes::BlockMode;
-// use cipher::{BlockCipher, BlockEncrypt, BlockDecrypt}
-use cbc;
-use cipher::BlockEncryptMut;
-use cipher::BlockDecryptMut;
 use block_padding::Pkcs7;
-use cipher::KeyInit;
-use cipher::KeyIvInit;
+use cbc;
+use cipher::{BlockDecryptMut, BlockEncryptMut, KeyInit, KeyIvInit};
 use clear_on_drop::clear::Clear;
-use hmac::digest::OutputSizeUser;
-use ksi_double_ratchet::{self as dr, KeyPair as _};
-
 use generic_array::{typenum::U32, GenericArray};
 use hkdf::Hkdf;
+use hmac::digest::OutputSizeUser;
 use hmac::{Hmac, Mac};
+use ksi_double_ratchet::{self as dr, KeyPair as _};
 use rand_core::{CryptoRng, RngCore};
 use rand_os::OsRng;
 use sha2::Sha256;
 use std::fmt;
 use std::hash::{Hash, Hasher};
-// use subtle::ConstantTimeEq;
 use x25519_dalek::{self, SharedSecret};
 
 pub type SignalDR = dr::DoubleRatchet<SignalCryptoProvider>;
@@ -44,147 +34,131 @@ impl dr::CryptoProvider for SignalCryptoProvider {
     }
 
     fn kdf_rk(rk: &SymmetricKey, s: &SharedSecret) -> (SymmetricKey, SymmetricKey) {
-        let salt = Some(rk.0.as_slice());
-        let ikm = s.as_bytes();
-        let prk = Hkdf::<Sha256>::new(salt, ikm);
-        // let prk = Hkdf::<Sha256>::extract(salt, ikm);
+        let salt: Option<&[u8]> = Some(rk.0.as_slice());
+        let input_key_material: &[u8; 32] = s.as_bytes();
+        let pseudo_random_key = Hkdf::<Sha256>::new(salt, input_key_material);
+
         let info = &b"WhisperRatchet"[..];
-        let mut okm = [0; 64];
-        prk.expand(&info, &mut okm).unwrap();
+        let mut output_key_material = [0; 64];
+        pseudo_random_key.expand(&info, &mut output_key_material).unwrap();
 
-        let rk = GenericArray::<u8, U32>::from_slice(&okm[..32]).clone();
-        
-        let ck = GenericArray::<u8, U32>::from_slice(&okm[32..]).clone();
+        let root_key: GenericArray<u8, _> = GenericArray::<u8, U32>::from_slice(&output_key_material[..32]).clone();
 
-        (SymmetricKey(rk), SymmetricKey(ck))
+        let chain_key: GenericArray<u8, _> =
+            GenericArray::<u8, U32>::from_slice(&output_key_material[32..]).clone();
+
+        return (SymmetricKey(root_key), SymmetricKey(chain_key))
     }
 
     fn kdf_ck(ck: &SymmetricKey) -> (SymmetricKey, SymmetricKey) {
         let key = ck.0.as_slice();
-        
-        let mut mac = <Hmac::<Sha256> as KeyInit>::new_from_slice(key).unwrap();
+
+        let mut mac = <Hmac<Sha256> as KeyInit>::new_from_slice(key).unwrap();
         mac.update(&[0x01]);
-        let mk = mac.finalize().into_bytes();
+        let message_key = mac.finalize().into_bytes();
 
-        // TODO: check if this is correct, and we should not recreate the mac 
-        mac = <Hmac::<Sha256> as KeyInit>::new_from_slice(key).unwrap();
+        // TODO: [Kseen] check if this is correct, and we should not recreate the mac
+        mac = <Hmac<Sha256> as KeyInit>::new_from_slice(key).unwrap();
         mac.update(&[0x02]);
-        let ck = mac.finalize().into_bytes();
+        let chain_key = mac.finalize().into_bytes();
 
-        (SymmetricKey(ck), SymmetricKey(mk))
+        return (SymmetricKey(chain_key), SymmetricKey(message_key))
     }
 
     fn encrypt(key: &SymmetricKey, pt: &[u8], ad: &[u8]) -> Vec<u8> {
-        let ikm = key.0.as_slice();
-        let prk = Hkdf::<Sha256>::new(None, ikm);
-        // let prk = Hkdf::<Sha256>::extract(None, ikm);
+        let input_key_material = key.0.as_slice();
+        let pseudo_random_key = Hkdf::<Sha256>::new(None, input_key_material);
+
         let info = b"WhisperMessageKeys";
-        let mut okm = [0; 80];
-        prk.expand(info, &mut okm).unwrap();
+        let mut output_key_material = [0; 80];
+        pseudo_random_key.expand(info, &mut output_key_material).unwrap();
 
-        // let ek = GenericArray::<u8, <Aes256 as BlockCipher>::KeySize>::from_slice(&okm[..32]);
-        let ek = GenericArray::<u8, <Aes256 as KeySizeUser>::KeySize>::from_slice(&okm[..32]);
-        let mk = GenericArray::<u8, <Hmac<Sha256> as OutputSizeUser>::OutputSize>::from_slice(&okm[32..64]);
-        // let iv = GenericArray::<u8, <Aes256 as BlockCipher>::BlockSize>::from_slice(&okm[64..]);
-        let iv = GenericArray::<u8, <Aes256 as BlockSizeUser>::BlockSize>::from_slice(&okm[64..]);
+        let encryption_key = GenericArray::<u8, <Aes256 as KeySizeUser>::KeySize>::from_slice(&output_key_material[..32]);
+        let message_key = GenericArray::<u8, <Hmac<Sha256> as OutputSizeUser>::OutputSize>::from_slice(
+            &output_key_material[32..64],
+        );
+        let iv = GenericArray::<u8, <Aes256 as BlockSizeUser>::BlockSize>::from_slice(&output_key_material[64..]);
 
+        type Aes256CbcEnc = cbc::Encryptor<Aes256>;
+        let ciphr = Aes256CbcEnc::new(encryption_key, iv);
+
+        let mut mod_plaintext = pt.to_vec();
+        let plaintext_len = pt.len();
+
+        mod_plaintext.resize(plaintext_len + 16 - (plaintext_len % 16), 0);
+
+        let ciphertext = match ciphr.encrypt_padded_mut::<Pkcs7>(&mut mod_plaintext, plaintext_len) {
+            Ok(encrypted) => encrypted,
+            Err(e) => panic!("Error: {:?}", e),
+        };
+
+        let mut mac = <Hmac<Sha256> as Mac>::new_from_slice(message_key).unwrap();
         
-        // let cipher = Cbc::<Aes256, Pkcs7>::new_fix(ek, iv);
-        type Aes256CbcEnc = cbc::Encryptor::<Aes256>;
-        let ciphr = Aes256CbcEnc::new(ek, iv);
-
-        let mut mpt = pt.to_vec();
-        let pt_len = pt.len();
-        println!("pt: {:?}", pt);
-        println!("pt: {:?}", pt_len);
-        // resize the buffer to have enough space for message and padding
-        mpt.resize(pt_len + 16 - (pt_len % 16), 0);
-        // let ct = Aes128CbcEnc::new(&key.into(), &iv.into())
-        // .encrypt_padded_mut::<Pkcs7>(&mut buf, pt_len)
-        // .unwrap();
-
-        let ciphertext = match ciphr
-            .encrypt_padded_mut::<Pkcs7>(&mut mpt, pt_len) {
-                Ok(encrypted) => encrypted,
-                Err(e) => panic!("Error: {:?}", e)
-            };
-        // let mut ct = cipher.encrypt_vec(pt);
-
-        let mut mac = <Hmac::<Sha256> as Mac>::new_from_slice(mk).unwrap();
-        // let mut mac = Hmac::<Sha256>::new_varkey(mk).unwrap();
         mac.update(ad);
         mac.update(&ciphertext);
+
         let tag = mac.finalize().into_bytes();
-        let mut ct_vec = ciphertext.to_vec();
+        let mut ciphertext_vec = ciphertext.to_vec();
 
-        println!("Clear ct:");
-        println!("ct: {:?}", ct_vec);
-        println!("tag: {:?}", &tag[..8]);
-        ct_vec.extend((&tag[..8]).into_iter());
+        ciphertext_vec.extend((&tag[..8]).into_iter());
 
-        okm.clear();
+        output_key_material.clear();
 
-        println!("ct: {:?}", ct_vec);
+        println!("ct: {:?}", ciphertext_vec);
 
-        ct_vec
+        ciphertext_vec
     }
 
     fn decrypt(key: &SymmetricKey, ct: &[u8], ad: &[u8]) -> Result<Vec<u8>, dr::DecryptError> {
-        let ikm = key.0.as_slice();
-        let prk = Hkdf::<Sha256>::new(None, ikm);
+        let input_key_material = key.0.as_slice();
+        let pseudo_random_key = Hkdf::<Sha256>::new(None, input_key_material);
         let info = b"WhisperMessageKeys";
-        let mut okm = [0; 80];
-        match prk.expand(info, &mut okm) {
+        let mut output_key_material = [0; 80];
+        match pseudo_random_key.expand(info, &mut output_key_material) {
             Ok(_) => (),
-            Err(e) => panic!("Error: {:?}", e)
+            Err(e) => panic!("Error: {:?}", e),
         };
-        let dk = GenericArray::<u8, <Aes256 as KeySizeUser>::KeySize>::from_slice(&okm[..32]);
-        let mk = GenericArray::<u8, <Hmac<Sha256> as OutputSizeUser>::OutputSize>::from_slice(&okm[32..64]);
-        let iv = GenericArray::<u8, <Aes256 as BlockSizeUser>::BlockSize>::from_slice(&okm[64..]);
+        let decryption_key = GenericArray::<u8, <Aes256 as KeySizeUser>::KeySize>::from_slice(&output_key_material[..32]);
+        let message_key = GenericArray::<u8, <Hmac<Sha256> as OutputSizeUser>::OutputSize>::from_slice(
+            &output_key_material[32..64],
+        );
+        let iv = GenericArray::<u8, <Aes256 as BlockSizeUser>::BlockSize>::from_slice(&output_key_material[64..]);
 
-        println!("ct: {:?}", ct);
-        let ct_len_wo_tag = ct.len() - 8;
-        println!("ct_len_wo_tag: {:?}", ct_len_wo_tag);
-        let mut mac = match <Hmac::<Sha256> as Mac>::new_from_slice(mk) {
+        let ciphertext_len = ct.len() - 8;
+
+        let mut mac = match <Hmac<Sha256> as Mac>::new_from_slice(message_key) {
             Ok(mac) => mac,
-            Err(e) => panic!("Error: {:?}", e)
+            Err(e) => panic!("Error: {:?}", e),
         };
+
         mac.update(ad);
-        mac.update(&ct[..ct_len_wo_tag]);
+        mac.update(&ct[..ciphertext_len]);
+        
         let tag = mac.finalize().into_bytes();
 
-        // if bool::from(!(&tag.as_ref()[..8]).ct_eq(&ct[ct_len..])) {
-        // This seems like a better way to compare the two slices??
-        // println!("tag: {:?}", &tag.as_ref()[..8]);
-        println!("tag: {:?}", &tag[..8]);
-        println!("ct: {:?}", &ct[(ct_len_wo_tag)..]);
-
-        if bool::from(!((&tag[..8]) == (&ct[ct_len_wo_tag..]))) {
-            okm.clear();
+        if bool::from(!((&tag[..8]) == (&ct[ciphertext_len..]))) {
+            output_key_material.clear();
             println!("Error: {:?}", dr::DecryptError::DecryptFailure);
             return Err(dr::DecryptError::DecryptFailure);
         }
 
-        type Aes256CbcDec = cbc::Decryptor::<Aes256>;
-        let ciphr = Aes256CbcDec::new(dk, iv);
+        type Aes256CbcDec = cbc::Decryptor<Aes256>;
+        let ciphr = Aes256CbcDec::new(decryption_key, iv);
 
-        let mut ct_vec = ct[..ct_len_wo_tag].to_vec();
-        
-        println!("ct_vec: {:?}", ct_vec);
+        let mut ciphertext_vec = ct[..ciphertext_len].to_vec();
 
-        return match ciphr
-            .decrypt_padded_mut::<Pkcs7>(&mut ct_vec) {
-                Ok(pt) => {
-                    okm.clear();
-                    println!("pt: {:?}", pt);
-                    Ok(pt.to_vec())
-                },
-                Err(e) => {
-                    okm.clear();
-                    println!("Error: {:?}", e);
-                    Err(dr::DecryptError::DecryptFailure)
-                }
-            };
+        return match ciphr.decrypt_padded_mut::<Pkcs7>(&mut ciphertext_vec) {
+            Ok(pt) => {
+                output_key_material.clear();
+                println!("pt: {:?}", pt);
+                Ok(pt.to_vec())
+            }
+            Err(e) => {
+                output_key_material.clear();
+                println!("Error: {:?}", e);
+                Err(dr::DecryptError::DecryptFailure)
+            }
+        };
     }
 }
 
@@ -281,7 +255,7 @@ impl Drop for SymmetricKey {
 #[test]
 fn signal_session() {
     let mut rng = OsRng::new().unwrap();
-    let (ad_a, ad_b) = (b"A2B:SessionID=42", b"B2A:SessionID=42");
+    let (ad_a, ad_b) = (b"A2B:SessionID=42", b"B2A:SessionID=42"); // Authentication data
 
     // Copy some values (these are usually the outcome of an X3DH key exchange)
     let bobs_prekey = KeyPair::new(&mut rng);
@@ -292,8 +266,10 @@ fn signal_session() {
 
     // Alice fetches Bob's prekey bundle and completes her side of the X3DH handshake
     let mut alice = SignalDR::new_alice(&shared, bobs_public_prekey, None, &mut rng);
+
     // Alice creates her first message to Bob
     let pt_a_0 = b"Hello Bob";
+    
     let (h_a_0, ct_a_0) = alice.ratchet_encrypt(pt_a_0, ad_a, &mut rng);
     // Alice creates an initial message containing `h_a_0`, `ct_a_0` and other X3DH information
 
